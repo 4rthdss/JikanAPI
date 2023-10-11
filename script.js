7b31a06017cf126d48e7c0ac8ee602c4191d2419
Arthur Silva <102180434+4rthdss@users.noreply.github.com>
2023-10-11 19:59:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function getSeason() {
+    let seasonUrl = `https://api.jikan.moe/v4/seasons/now`
+    fetch(seasonUrl)
+    .then(response => response.json())
+    .then(data => {
+        let node = document.getElementById('search_results');
+        while (node.firstChild) {node.removeChild(node.firstChild);}
+            data.data.forEach(item => {
+                document.getElementById('search_results')
+                .insertAdjacentHTML(
+                    'beforeend',
+                    `
+                    <span>${item.title}</span>
+                        <div class="card__image">
+                            <img width="230" heigth="325" src="${item.images.jpg.large_image_url}" alt="${item.title}" />
+                        </div>
+                        <br> 
+                    `
+                );
+            });
+    });
+
+}
+getSeason()
+
+function search() {
+    let query = document.getElementById('search_query').value;
+
+    let requestUrl = `https://api.jikan.moe/v4/anime?q=${query}&sfw`;
+
+
+    fetch(requestUrl)
+    .then(response => response.json())
+    .then(data => {
+        let node = document.getElementById('search_results');
+        while (node.firstChild) {node.removeChild(node.firstChild);}
+            data.data.forEach(item => {
+                document.getElementById('search_results')
+                .insertAdjacentHTML(
+                    'beforeend',
+                    `
+                    <span>${item.title}</span>
+                        <div class="card__image">
+                            <img width="230" heigth="325" src="${item.images.jpg.large_image_url}" alt="${item.title}" />
+                        </div>
+                        <br> 
+                    `
+                );
+            });
+    });
+}
+
+document.getElementById('search').onclick = () => search();
+document.getElementById('search_query').onkeydown = (event) => {
+    if (event.keyCode === 13) {
+        search();
+    }
+};
